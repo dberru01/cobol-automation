9aa2de4e6e6e7d3c76dd1603345b4064efa5c797
Daniel Berru <40674086+dberru01@users.noreply.github.com>
2025-04-07 12:08:53 -0500

    Update zowe_operations.sh

--- .github/scripts/zowe_operations.sh
+++ .github/scripts/zowe_operations.sh
@@ -5,17 +5,23 @@
 # Convert username to lowercase
 LOWERCASE_USERNAME=$(echo "$ZOWE_USERNAME" | tr '[:upper:]' '[:lower:]')
 
+echo "Usename:"
+echo $LOWERCASE_USERNAME
+
 # Check if directory exists, create if it doesn't
+echo "Checking directory..."
+
 if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" &>/dev/null; then
   echo "Directory does not exist. Creating it..."
-  zowe zos-files create uss-directory /z/$LOWERCASE_USERNAME/cobolcheck
+  zowe zos-files create uss-directory "/z/$LOWERCASE_USERNAME/cobolcheck"
 else
   echo "Directory already exists."
 fi
 
 # Upload files
-zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck" --recursive --binary-files "cobol-check-0.2.17.jar"
+echo "Uploading files..."
+zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck" --recursive --binary-files "cobol-check-0.2.16.jar"
 
 # Verify upload
-echo "Verifying upload:"
+echo "Verifying upload..."
 zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck"
